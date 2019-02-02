94d62bbe014daf4bb15ecb2118076f2042199100
ابرار <abrar@macbook-pro-alkhas-b-abrar.local>
2019-02-02 18:46:55 +0300

    Initial Commit

---
+++ On the Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by ابرار on ٢٧ جما١، ١٤٤٠ هـ.
+//  Copyright © ١٤٤٠ هـ Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
